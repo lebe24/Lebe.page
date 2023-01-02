0c3bacaa3fd9cd0462752aa0957f0fa0637c2eb5
lebe24 <emmanuel.philipel@yahoo.com>
2023-01-02 17:55:56 +0100

    updates

--- js/main.js
+++ js/main.js
@@ -31,7 +31,6 @@ $(function () {
   const onscroll = (el, listener) => {
     el.addEventListener('scroll', listener)
   }
-
       // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
   strings: ["fullstack developer", "Mobile developer","Blockchain Developer","Tech Lover"],
@@ -39,6 +38,75 @@ var typed = new Typed(".typing-text", {
   typeSpeed: 50,
   backSpeed: 25,
   backDelay: 500,
+});
+
+/**
+   * Navbar links active state on scroll
+   */
+let navbarlinks = select('#navbar .scrollto', true)
+const navbarlinksActive = () => {
+  let position = window.scrollY + 200
+  navbarlinks.forEach(navbarlink => {
+    if (!navbarlink.hash) return
+    let section = select(navbarlink.hash)
+    if (!section) return
+    if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
+      navbarlink.classList.add('active')
+    } else {
+      navbarlink.classList.remove('active')
+    }
+  })
+}
+window.addEventListener('load', navbarlinksActive)
+onscroll(document, navbarlinksActive)
+
+
+/**
+   * Scrolls to an element with header offset
+   */
+const scrollto = (el) => {
+  let header = select('#header')
+  let offset = header.offsetHeight
+
+  if (!header.classList.contains('header-scrolled')) {
+    offset -= 10
+  }
+
+  let elementPos = select(el).offsetTop
+  window.scrollTo({
+    top: elementPos - offset,
+    behavior: 'smooth'
+  })
+}
+
+
+/**
+   * Scrool with ofset on links with a class name .scrollto
+   */
+on('click', '.scrollto', function(e) {
+  if (select(this.hash)) {
+    e.preventDefault()
+
+    let navbar = select('#navbar')
+    if (navbar.classList.contains('navbar-mobile')) {
+      navbar.classList.remove('navbar-mobile')
+      let navbarToggle = select('.mobile-nav-toggle')
+      navbarToggle.classList.toggle('bi-list')
+      navbarToggle.classList.toggle('bi-x')
+    }
+    scrollto(this.hash)
+  }
+}, true)
+
+/**
+ * Scroll with ofset on page load with hash links in the url
+ */
+window.addEventListener('load', () => {
+  if (window.location.hash) {
+    if (select(window.location.hash)) {
+      scrollto(window.location.hash)
+    }
+  }
 });
   
   /**
