cf6641945072e48e2201124baa11e726490c669d
Emmanuel Philip <31292486+lebe24@users.noreply.github.com>
2023-01-15 13:46:19 +0100

    css updates

--- js/main.js
+++ js/main.js
@@ -1,10 +1,7 @@
-$(function () {
+(function() {
 
   "use strict";
 
-  /**
-   * Easy selector helper function
-   */
   const select = (el, all = false) => {
     el = el.trim()
     if (all) {
@@ -31,92 +28,60 @@ $(function () {
   const onscroll = (el, listener) => {
     el.addEventListener('scroll', listener)
   }
-      // <!-- typed js effect starts -->
-var typed = new Typed(".typing-text", {
-  strings: ["fullstack developer", "Mobile developer","Blockchain Developer","Tech Lover"],
-  loop: true,
-  typeSpeed: 50,
-  backSpeed: 25,
-  backDelay: 500,
-});
 
-/**
+  /**
    * Navbar links active state on scroll
    */
-let navbarlinks = select('#navbar .scrollto', true)
-const navbarlinksActive = () => {
-  let position = window.scrollY + 200
-  navbarlinks.forEach(navbarlink => {
-    if (!navbarlink.hash) return
-    let section = select(navbarlink.hash)
-    if (!section) return
-    if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
-      navbarlink.classList.add('active')
-    } else {
-      navbarlink.classList.remove('active')
-    }
-  })
-}
-window.addEventListener('load', navbarlinksActive)
-onscroll(document, navbarlinksActive)
+  let navbarlinks = select('#navbar .scrollto', true)
+  const navbarlinksActive = () => {
+    let position = window.scrollY + 200
+    navbarlinks.forEach(navbarlink => {
+      if (!navbarlink.hash) return
+      let section = select(navbarlink.hash)
+      if (!section) return
+      if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
+        navbarlink.classList.add('active')
+      } else {
+        navbarlink.classList.remove('active')
+      }
+    })
+  }
+  window.addEventListener('load', navbarlinksActive)
+  onscroll(document, navbarlinksActive)
 
-
-/**
+  /**
    * Scrolls to an element with header offset
    */
-const scrollto = (el) => {
-  let header = select('#header')
-  let offset = header.offsetHeight
+  const scrollto = (el) => {
+    let header = select('#header')
+    let offset = header.offsetHeight
 
-  if (!header.classList.contains('header-scrolled')) {
-    offset -= 10
-  }
-
-  let elementPos = select(el).offsetTop
-  window.scrollTo({
-    top: elementPos - offset,
-    behavior: 'smooth'
-  })
-}
-
-
-/**
-   * Scrool with ofset on links with a class name .scrollto
-   */
-on('click', '.scrollto', function(e) {
-  if (select(this.hash)) {
-    e.preventDefault()
-
-    let navbar = select('#navbar')
-    if (navbar.classList.contains('navbar-mobile')) {
-      navbar.classList.remove('navbar-mobile')
-      let navbarToggle = select('.mobile-nav-toggle')
-      navbarToggle.classList.toggle('bi-list')
-      navbarToggle.classList.toggle('bi-x')
+    if (!header.classList.contains('header-scrolled')) {
+      offset -= 10
     }
-    scrollto(this.hash)
-  }
-}, true)
 
-/**
- * Scroll with ofset on page load with hash links in the url
- */
-window.addEventListener('load', () => {
-  if (window.location.hash) {
-    if (select(window.location.hash)) {
-      scrollto(window.location.hash)
-    }
+    let elementPos = select(el).offsetTop
+    window.scrollTo({
+      top: elementPos - offset,
+      behavior: 'smooth'
+    })
   }
-});
-  
+
   /**
-   * Mobile nav toggle
+   * Toggle .header-scrolled class to #header when page is scrolled
    */
-  on('click', '.mobile-nav-toggle', function(e) {
-    select('#navbar').classList.toggle('navbar-mobile')
-    this.classList.toggle('bi-list')
-    this.classList.toggle('bi-x')
-  })
+  let selectHeader = select('#header')
+  if (selectHeader) {
+    const headerScrolled = () => {
+      if (window.scrollY > 100) {
+        selectHeader.classList.add('header-scrolled')
+      } else {
+        selectHeader.classList.remove('header-scrolled')
+      }
+    }
+    window.addEventListener('load', headerScrolled)
+    onscroll(document, headerScrolled)
+  }
 
   /**
    * Scrool with ofset on links with a class name .scrollto
@@ -135,5 +100,31 @@ window.addEventListener('load', () => {
       scrollto(this.hash)
     }
   }, true)
-  
-})
+
+  window.addEventListener('load', () => {
+    if (window.location.hash) {
+      if (select(window.location.hash)) {
+        scrollto(window.location.hash)
+      }
+    }
+  });
+
+  /**
+     * Mobile nav toggle
+     */
+  on('click', '.mobile-nav-toggle', function(e) {
+    select('#navbar').classList.toggle('navbar-mobile')
+    this.classList.toggle('bi-list')
+    this.classList.toggle('bi-x')
+  })
+
+  var typed = new Typed(".typing-text", {
+    strings: ["Fullstack developer", "Fullstack  Mobile","Fullstack  Web","Blockchain Developer","Tech Lover"],
+    loop: true,
+    typeSpeed: 50,
+    backSpeed: 25,
+    backDelay: 500,
+  });
+
+
+})();
